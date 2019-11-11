67ec4edbddbcbd48fdba354eb7aff5dcc73a1f47
Seraphim <freedom1040425@gmail.com>
2019-11-11 10:16:03 +0800

    iiiAndroid004

---
+++ app/src/main/java/tw/org/iii/iiiandroid004/MainActivity.java
@@ -0,0 +1,29 @@
+package tw.org.iii.iiiandroid004;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void exit(View view) {
+    }
+
+    public void setting(View view) {
+    }
+
+    public void newGame(View view) {
+    }
+
+    public void guess(View view) {
+    }
+}
