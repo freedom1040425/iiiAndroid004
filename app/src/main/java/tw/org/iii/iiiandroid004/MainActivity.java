5cc080f1cf8862b9a1f2e00cc4fcaadcb1f761e7
Seraphim <freedom1040425@gmail.com>
2019-11-11 11:33:12 +0800

    iiiAndroid004

--- app/src/main/java/tw/org/iii/iiiandroid004/MainActivity.java
+++ app/src/main/java/tw/org/iii/iiiandroid004/MainActivity.java
@@ -3,8 +3,11 @@ package tw.org.iii.iiiandroid004;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
+
+import java.util.HashSet;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +16,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    //產生謎底
+    private String creatAnswer(int dig){
+        HashSet<Integer> set = new HashSet<>();
+        while (set.size()<dig){
+            set.add((int)(Math.random()*10));
+        }
+        StringBuffer sb = new StringBuffer();
+        for(Integer i :set){
+            sb.append(i);
 
+        }
+
+        Log.v("brad", sb.toString());
+        return sb.toString();
+    }
 
     public void exit(View view) {
+        creatAnswer(4);
     }
 
     public void setting(View view) {
